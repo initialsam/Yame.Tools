4f17eeb58d7d5a2e55137ae060defc786859a58f
sam <evaoosyz@gmail.com>
2020-03-27 14:49:53 +0800

    加入 INotifyPropertyChanged 範例

---
+++ Yame.XUnitTests/NotifyPropertyChanged.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Yame.XUnitTests
+{
+    public class NotifyPropertyChanged
+    {
+        private readonly ITestOutputHelper _output;
+
+        public NotifyPropertyChanged(ITestOutputHelper testOutputHelper)
+        {
+            _output = testOutputHelper;
+        }
+        [Fact]
+        public void NotifyWhenNameChanged()
+        {
+            Person sut = new Person();
+            sut.PropertyChanged += OnPropertyChanged;
+            Assert.PropertyChanged(sut, "Name", () => sut.Name = "Amrit");
+            //Property Name was changed.
+        }
+
+
+        [Fact]
+        public void NotifyWhenAgeChanged()
+        {
+            Person sut = new Person();
+            sut.PropertyChanged += OnPropertyChanged;
+            Assert.PropertyChanged(sut, "Age", () => sut.Age = 42);
+            //Property Age was changed.
+        }
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            _output.WriteLine($"Property {e.PropertyName} was changed.");
+        }
+    }
+
+    public class Person : INotifyPropertyChanged
+    {
+        private int _age;
+        private string _name;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                _age = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        protected virtual void OnPropertyChanged(
+            [CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
