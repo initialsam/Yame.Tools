e455d4abd40c69e4ee3ede894f3e12721e9575b0
sam <evaoosyz@gmail.com>
2020-04-26 16:35:58 +0800

    加入 測試MongoDB

--- WebApi2/Controllers/ValuesController.cs
+++ WebApi2/Controllers/ValuesController.cs
@@ -1,17 +1,50 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApi2.Models;
 
 namespace WebApi2.Controllers
 {
     public class ValuesController : ApiController
     {
+
         // GET api/values
         public IEnumerable<string> Get()
         {
+            var dbClient = new MongoClient("mongodb://127.0.0.1:27017");
+            var dbList = dbClient.ListDatabases().ToList();
+            IMongoDatabase db = dbClient.GetDatabase("cms");
+            var command = new BsonDocument { { "dbstats", 1 } };
+            var result = db.RunCommand<BsonDocument>(command);
+
+         
+            var cars = db.GetCollection<Book>(nameof(Book));
+
+            var doc = new Book
+            {
+                BookName = "TT",
+                BookType = BookType.cc,
+                Price = 100
+            };
+
+            cars.InsertOne(doc);
+
+            var builder = Builders<Book>.Filter;
+            var filter = builder.Gt("Price", 10) & builder.Lt("Price", 1000);
+
+            var docs = cars.Find(filter).ToList();
+
+            Console.WriteLine("The list of databases are:");
+
+            foreach (var item in dbList)
+            {
+                Console.WriteLine(item);
+            }
             return new string[] { "value1", "value2" };
         }
 
---
+++ WebApi2/Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi2.Models
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string BookName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public BookType BookType { get; set; }
+    }
+
+    public enum BookType
+    {
+        aa,bb,cc
+    }
+
+
+}
