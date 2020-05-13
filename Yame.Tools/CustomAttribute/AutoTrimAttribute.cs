5e72bee7c6941392c2de28c66fb13dc7f61beaa5
sam <evaoosyz@gmail.com>
2020-05-13 21:39:49 +0800

    Yame.Tools 加入 CustomAttribute 範例

---
+++ Yame.Tools/CustomAttribute/AutoTrimAttribute.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Yame.Tools.CustomAttribute
+{
+    /*
+    [單元測試]0800自動截斷屬性值
+    https://youtu.be/S947NenkvqA
+    */
+    public static class StringHelper
+    {
+        public static void AutoTrim<T>(T source) where T : class
+        {
+            if (source == null) return;
+
+            var targetProperties = GetAutoTrimProperties(typeof(T));
+            targetProperties.ForEach(x => TryPurgeString(x, source));
+        }
+
+        private static void TryPurgeString<T>(PropertyInfo prop,T source)
+        {
+            object oValue = prop.GetValue(source);
+            if (oValue == null) return;
+
+            if (prop.PropertyType != typeof(string)) return;
+
+            string value = oValue.ToString();
+            int maxLength = ((AutoTrimAttribute) prop.GetCustomAttributes(typeof(AutoTrimAttribute), true)[0])
+                .MaxLength;
+            if (value.Length <= maxLength) return;
+
+            prop.SetValue(source, value.Substring(0, maxLength));
+        }
+
+        private static List<PropertyInfo> GetAutoTrimProperties(Type sourceType)
+        {
+            return sourceType.GetProperties()
+                             .Where(x => x.GetCustomAttributes(typeof(AutoTrimAttribute), true).Length == 1)
+                             .ToList();
+        }
+    }
+
+    [AttributeUsage( AttributeTargets.Property,AllowMultiple =false)]
+    public class AutoTrimAttribute : Attribute
+    {
+        public int MaxLength { get; }
+        public AutoTrimAttribute(int maxLength)
+        {
+            MaxLength = maxLength;
+        }
+    }
+}
---
+++ Yame.ToolsTests/CustomAttribute/StringHelperTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yame.Tools.CustomAttribute;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+
+namespace Yame.Tools.CustomAttribute.Tests
+{
+    [TestClass()]
+    public class StringHelperTests
+    {
+        [TestMethod()]
+        public void AutoTrim_字串過長_截斷屬性值()
+        {
+            //Arrange
+            var fakeClass = new FakeClass { Name = "AAAAAAA" };
+            var actual = "AAA"; 
+            //Act
+            StringHelper.AutoTrim(fakeClass);
+            //Assert
+            actual.Should().Be(fakeClass.Name);
+        }
+
+        [TestMethod()]
+        public void AutoTrim_字串太短或null_不處理()
+        {
+            //Arrange
+            var fakeClass = new FakeClass { Name = "AA" };
+            var actual = "AA";
+            //Act
+            StringHelper.AutoTrim(fakeClass);
+            //Assert
+            actual.Should().Be(fakeClass.Name);
+
+            //Arrange
+            var fakeClass2 = new FakeClass();
+            //Act
+            StringHelper.AutoTrim(fakeClass2);
+            //Assert
+            fakeClass2.Name.Should().BeNull();
+        }
+    }
+
+    public class FakeClass
+    {
+        [AutoTrim(3)]
+        public string Name { get; set; }
+    }
+
+}
