7f5388fd1391c592c0a2134da70717a137f4c2a9
g-Samlin <g-samlin@gamania.com>
2020-05-21 19:05:47 +0800

    Test Version

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -11,17 +11,40 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("MachineName: {0}", Environment.MachineName);
+            var localVersion = new Version("2.30.0");
+            var serverVersion = new Version("2.28.12");
+            var result1 = serverVersion.CompareTo(localVersion); //-1   小於
 
-            string HostName = Dns.GetHostName();
-            Console.WriteLine("Host Name of machine =" + HostName);
-            IPAddress[] ipaddress = Dns.GetHostAddresses(HostName);
-            Console.Write("IPv4 of Machine is ");
-            foreach (IPAddress ip4 in ipaddress.Where(ip => ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork))
+            var result2= localVersion.CompareTo(serverVersion); //1     大於
+
+            var aa = new Version("2.28.12");
+            var bb = new Version("2.28.12");
+            var result3 = aa.CompareTo(bb); //0  等於          
+
+
+            var aaaa = CheckAppVersion("2.30.0", "2.29.1"); //1
+            var bbbb = CheckAppVersion("2.30.0", "2.30.0"); //0
+            var cccc = CheckAppVersion("2.30.0", "2.30.1"); //-1
+
+
+        }
+
+
+        public static int CheckAppVersion(string limitVersion, string appVersion)
+        {
+            //如果傳入的 AccountID or appVersion 不正確，則直接傳回 false
+            if (string.IsNullOrEmpty(appVersion))
             {
-                Console.WriteLine(ip4.ToString());
+                return -1;
             }
-            Console.ReadKey();
+            //比較版本，若目前APP版本小於limitVersion則回傳true
+            Version Version_limitVersion = new Version(limitVersion); //這支API想判斷的APP版本
+            Version Version_appVersion = new Version(appVersion);     //目前用戶的APP版本
+
+            //Version_limitVersion == Version_appVersion => 0 等於
+            //Version_limitVersion >  Version_appVersion => 1 大於
+            //Version_limitVersion <  Version_appVersion => -1 小於
+            return Version_limitVersion.CompareTo(Version_appVersion);
         }
     }
 }
