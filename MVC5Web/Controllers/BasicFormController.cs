4479b57c6872b5a6c1c04726ed5d338c614e9996
sam <evaoosyz@gmail.com>
2020-05-23 21:51:09 +0800

    加入 MVC5 專案 跟 升級Bootstarp 4.5

---
+++ MVC5Web/Controllers/BasicFormController.cs
@@ -0,0 +1,29 @@
+﻿using MVC5Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Web.Controllers
+{
+    public class BasicFormController : Controller
+    {
+        // GET: BasicForm
+        public ActionResult Index()
+        {
+            var modelList = new List<Visit>()
+            {
+                new Visit{ PetName="aa" },
+                new Visit{ PetName="bb" },
+                new Visit{ PetName="cc" }
+            };
+            return View(modelList);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVC5Web/Models/Visit.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Web.Models
+{
+    public class Visit
+    {
+        public int ID { get; set; }
+        [Required]
+        [Display(Name = "Pet Name")]
+        public string PetName { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Visit Date")]
+        public DateTime VisitDate { get; set; }
+
+        //Single checkbox
+        [Display(Name = "Is this an emergency visit?")]
+        public bool IsEmergencyVisit { get; set; }
+
+        //Textarea
+        [DataType(DataType.MultilineText)]
+        public string Notes { get; set; }
+
+        //Radio button
+        //could also be string, bool, or double, depending on value type
+        [Display(Name = "Veterinarian")]
+        public int VeterinarianID { get; set; }
+
+        //Number
+        public int Cost { get; set; }
+
+        //Range
+        [Display(Name = "Pet Weight")]
+        public double PetWeight { get; set; }
+
+        //Tel
+        [Phone]
+        public string CustomerPhone { get; set; }
+    }
+}
