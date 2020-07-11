755b6d621b117e8f256c7b4749cb627cacb88a3f
sam <evaoosyz@gmail.com>
2020-07-11 23:32:09 +0800

    MVC5 Web 加入 Ajax.BeginForm 跟 驗證失敗 js 顯示錯誤

--- MVC5Web/Controllers/BasicFormController.cs
+++ MVC5Web/Controllers/BasicFormController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -36,9 +37,44 @@ namespace MVC5Web.Controllers
             return View(viewModel);
         }
 
+        public ActionResult CreateAjax()
+        {
+            return Create();
+        }
+
+        [HttpPost]
+        public ActionResult CreateAjax(Visit viewModel)
+        {
+            if (!ModelState.IsValid) return ReturnError();
+            try
+            {
+                ModelState.AddModelError("PetName", "PetName error set backend");
+                throw new Exception("Test Ajax Fail");
+            }
+            catch (Exception ex)
+            {
+                //ModelState.AddModelError(string.Empty, ex.Message);
+                return ReturnError();
+            }
+            return View(viewModel);
+        }
+
         public ActionResult GeneralForms()
         {
             return View();
+        }
+
+        private ActionResult ReturnError()
+        {
+            Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            Response.TrySkipIisCustomErrors = true;
+            var returnData = new
+            {
+                ModelStateErrors = ModelState.Where(x => x.Value.Errors.Count > 0)
+                                             .ToDictionary(k => k.Key,
+                                                           k => k.Value.Errors.Select(e => e.ErrorMessage).ToArray())
+            };
+            return Json(returnData);
         }
     }
 }
