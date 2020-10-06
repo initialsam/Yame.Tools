c31ec0f15ab4cb38bd85583e8147326b956facac
g-Samlin <g-samlin@gamania.com>
2020-10-06 18:29:16 +0800

    加入 EnumerableExtension => Distinct

---
+++ Yame.ToolsTests/Dto/DemoProduct.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Yame.Tools.Helper;
+
+namespace Yame.ToolsTests.Dto
+{
+    internal class DemoProduct
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
