1c4f1f226ec3d959697720ea9676ddfb3ce310c8
sam <evaoosyz@gmail.com>
2022-07-24 16:55:01 +0800

    加入 FeatureToggleService 並測試 lock

---
+++ CSharpLab/FeatureToggleService.cs
@@ -0,0 +1,105 @@
+﻿using CSharpLab.A06_Cache;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace CSharpLab
+{
+    public class FeatureToggleService
+    {
+        private readonly static object _lock = new object();
+        private List<FeatureToggle> _featureToggleList;
+        private IFeatureToggleRepo _featureToggleRepo;
+
+        public FeatureToggleService(IFeatureToggleRepo featureToggleRepo)
+        {
+            _featureToggleRepo = featureToggleRepo;
+        }
+        private List<FeatureToggle> GetCacheWithLock()
+        {
+            lock (_lock)
+            {
+
+                if (_featureToggleList != null)
+                {
+                    return _featureToggleList;
+                }
+                Thread.Sleep(1000);
+                _featureToggleList = UpdateCache();
+            }
+
+            return _featureToggleList;
+        }
+
+        public FeatureToggle GetFeatureToggle(string code)
+        {
+            CheckData();
+            var featureToggle = _featureToggleList.FirstOrDefault(x => x.Code == code);
+            return featureToggle ?? new FeatureToggle { Status = FeatureToggleStatusType.Close };
+        }
+
+        public List<FeatureToggle> UpdateCache()
+        {
+
+            return _featureToggleRepo.GetAllByDatabase();
+        }
+        private void CheckData()
+        {
+            if (_featureToggleList == null)
+            {
+                _featureToggleList = GetCacheWithLock();
+            }
+        }
+    }
+
+    public interface IFeatureToggleRepo
+    {
+        List<FeatureToggle> GetAllByDatabase();
+    }
+
+    public class FeatureToggle
+    {
+        public long RecordId { get; set; }
+        public string Name { get; set; }
+        public FeatureToggleStatusType Status { get; set; }
+        /// <summary>
+        /// 測試人員 Member Id 用逗號分隔
+        /// </summary>
+        public string TesterMemberIdList { get; set; }
+
+        public string UpdateBy { get; set; }
+
+        public DateTime UpdateTime { get; set; }
+
+
+        public string Code { get; set; }
+
+
+    }
+
+    public enum FeatureToggleStatusType
+    {
+        /// <summary>
+        /// 關閉
+        /// </summary>
+        [Description("關閉")]
+        Close = 0,
+        /// <summary>
+        /// 測試中
+        /// </summary>
+        [Description("測試中")]
+        Testing = 1,
+        /// <summary>
+        /// 開放
+        /// </summary>
+        [Description("開放")]
+        Open = 2
+    }
+}
---
+++ CSharpLabTests/FeatureToggleHelperTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpLab;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using FluentAssertions;
+using System.Threading;
+
+namespace CSharpLab.Tests
+{
+    [TestClass()]
+    public class FeatureToggleHelperTests
+    {
+        [TestMethod()]
+        public void GetFeatureToggleTest()
+        {
+            var featureToggleRepo = Substitute.For<IFeatureToggleRepo>();
+            featureToggleRepo.GetAllByDatabase().Returns(new List<FeatureToggle> { new FeatureToggle { Code = "A01" } });
+
+            var sut = new FeatureToggleService(featureToggleRepo);
+            Thread thread1 = new Thread(() => sut.GetFeatureToggle("A01"));
+            Thread thread2 = new Thread(() => sut.GetFeatureToggle("A01"));
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+            //var act = sut.GetFeatureToggle("A01");
+
+            featureToggleRepo.Received(1).GetAllByDatabase();
+          
+            //act.Code.Should().Be("A01");
+        }
+    }
+}
