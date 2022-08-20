dca63c56f261a618b3e9032dc03949b30b7d48da
sam <evaoosyz@gmail.com>
2022-08-20 14:17:04 +0800

    加上 CacheManager 單元測試

---
+++ CSharpLabTests/A06_Cache/CacheManagerTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CSharpLab.A06_Cache;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace CSharpLab.A06_Cache.Tests
+{
+    [TestClass()]
+    public class CacheManagerTests
+    {
+        [TestMethod()]
+        public void GetCachableDataTest()
+        {
+            var tasks = new List<Task>();
+            for (var i = 0; i < 2; i++)
+            {
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    var data = CacheManager.GetCachableData<string>("KEY",
+                        () =>
+                        {
+                            Console.WriteLine("Thread {0} Start Job",
+                                Thread.CurrentThread.ManagedThreadId);
+                            Thread.Sleep(3000);
+                            Console.WriteLine("Thread {0} Stop Job",
+                                Thread.CurrentThread.ManagedThreadId);
+                            return "OK";
+                        }, 10);
+                    Console.WriteLine("Data:" + data);
+                }));
+            }
+            tasks.ForEach(t => t.Wait());
+
+            Console.WriteLine("Done");
+            
+        }
+    }
+}
